8b9118f25ce5f0b538c7b24dfcfd1e15a11b8680
Christian Vonrüti <christian@vonrueti.ch>
2014-03-28 20:47:39 +0100

    Remove static cases list

--- IntroDBProject/src/ch/ethz/inf/dbproject/model/DatastoreInterface.java
+++ IntroDBProject/src/ch/ethz/inf/dbproject/model/DatastoreInterface.java
@@ -19,21 +19,6 @@ import ch.ethz.inf.dbproject.model.meta.TableName;
  * will be very helpful for part 2 of the project.
  */
 public final class DatastoreInterface {
-
-	//FIXME This is a temporary list of cases that will be displayed until all the methods have been properly implemented
-	private final static Case[] staticCases = new Case[] { 
-			new Case(0, "Noise pollution..", "1287.9%", 10000), 
-			new Case(1, "Highway overspeed...", "54.7%", 250000),
-			new Case(2, "Money Laundring...", "1.2%", 1000000),
-			new Case(3, "Corruption...", "0.0%", 1000000000),
-		};
-	private final static List<Case> staticCaseList = new ArrayList<Case>();
-	static {
-		for (int i = 0; i < staticCases.length; i++) {
-			staticCaseList.add(staticCases[i]);
-		}
-	}
-	
 	private Connection sqlConnection;
 
 	public DatastoreInterface() {
@@ -41,17 +26,7 @@ public final class DatastoreInterface {
 	}
 	
 	public final Case getCaseById(final int id) {
-	
-		/**
-		 * TODO this method should return the case with the given id
-		 */
-		
-		if (id < staticCases.length) {
-			return staticCases[id];
-		} else {
-			return null;
-		}
-		
+		return null;
 	}
 	
 	public final <T extends Entity> List<T> getAll(Class<T> clazz) {
