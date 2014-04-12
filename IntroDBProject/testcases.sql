9804df1a1eaef57f663a505919b54bbb4fe5eecf
Nina Zinsli <ninazinsli@gmail.com>
2014-04-12 11:50:08 +0200

    add testcases

---
+++ IntroDBProject/testcases.sql
@@ -0,0 +1,10 @@
+  
+--
+-- Testdaten
+--  
+INSERT INTO `Case` (description, date, time, status, location)
+  values("Diebstahl", '2014-03-11', '12:31:00', "open", "Zurich");
+
+INSERT INTO `Case` (description, date, time, status, location)
+  values("Mord", '2014-03-11', '12:31:00', "open", "Basel");
+  
