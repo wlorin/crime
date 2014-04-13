c50a8514d88d5fad38aeb2e4219a928f09f1514d
Christian Vonrüti <christian@vonrueti.ch>
2014-04-13 12:49:57 +0200

    Add UserInputException for form errors

---
+++ IntroDBProject/src/ch/ethz/inf/dbproject/forms/UserInputException.java
@@ -0,0 +1,12 @@
+package ch.ethz.inf.dbproject.forms;
+
+public class UserInputException extends RuntimeException {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5271898423674477549L;
+
+	public UserInputException(String msg) {
+		super(msg);
+	}
+}
