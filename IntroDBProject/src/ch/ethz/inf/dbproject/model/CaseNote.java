37cd026fb7b26ccb4faae32a010d0e852e3534d5
Christian Vonrüti <christian@vonrueti.ch>
2014-04-13 21:24:05 +0200

    Add class case note

---
+++ IntroDBProject/src/ch/ethz/inf/dbproject/model/CaseNote.java
@@ -0,0 +1,25 @@
+package ch.ethz.inf.dbproject.model;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import ch.ethz.inf.dbproject.model.meta.Entity;
+
+public class CaseNote extends Note {
+	
+	final int caseId;
+
+	public CaseNote(final String username, final String comment, final int caseId) {
+		super(username, comment);
+		this.caseId = caseId;
+	}
+	
+	public CaseNote(ResultSet rs) throws SQLException {
+		super(rs);
+		caseId = rs.getInt("CaseId");
+	}
+
+	public int getCaseId() {
+		return caseId;
+	}
+}
