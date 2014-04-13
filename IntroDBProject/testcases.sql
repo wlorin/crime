1d964f6e05c8a0cd5c815095109754eaee9629cd
Nina Zinsli <ninazinsli@gmail.com>
2014-04-13 17:47:56 +0200

    Change testcases

--- IntroDBProject/testcases.sql
+++ IntroDBProject/testcases.sql
@@ -17,23 +17,23 @@ INSERT INTO `Crime` (Crime)
 --
 -- Testdaten
 --  
-INSERT INTO `Case` (crimeId, date, time, status, location)
-  SELECT Crime.CrimeId, '2014-03-11', '12:31:00', "open", "Zurich"
+INSERT INTO `Case` (crimeId, date, time, status, location, name)
+  SELECT Crime.CrimeId, '2014-03-11', '12:31:00', "open", "Zurich", "Case 1"
   FROM Crime
   WHERE Crime.Crime = "Diebstahl";
 
-INSERT INTO `Case` (crimeId, date, time, status, location)
-  SELECT Crime.CrimeId, '2014-03-11', '12:31:00', "open", "Basel"
+INSERT INTO `Case` (crimeId, date, time, status, location, name)
+  SELECT Crime.CrimeId, '2014-03-11', '12:31:00', "open", "Basel", "Case 2"
   FROM Crime
   WHERE Crime.Crime = "Mord";
   
-INSERT INTO `Case` (crimeId, date, time, status, location)
-  SELECT Crime.CrimeId, '2012-08-23', '23:00:00', "closed", "Aarau"
+INSERT INTO `Case` (crimeId, date, time, status, location, name)
+  SELECT Crime.CrimeId, '2012-08-23', '23:00:00', "closed", "Aarau", "Case 3"
   FROM Crime
   WHERE Crime.Crime = "Koerperverletzung";  
   
-INSERT INTO `Case` (crimeId, date, time, status, location)
-  SELECT Crime.CrimeId, '2014-01-13', '09:15:00', "open", "Zurich"
+INSERT INTO `Case` (crimeId, date, time, status, location, name)
+  SELECT Crime.CrimeId, '2014-01-13', '09:15:00', "open", "Zurich", "Case 4"
   FROM Crime
   WHERE Crime.Crime = "Bankraub";
   
