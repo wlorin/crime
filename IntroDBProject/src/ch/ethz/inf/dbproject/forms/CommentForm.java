0904b482b051ee088cf5cdf351a28e7bef6b9edc
Christian Vonrüti <christian@vonrueti.ch>
2014-04-13 22:34:22 +0200

    ^ Extract super class

--- IntroDBProject/src/ch/ethz/inf/dbproject/forms/CommentForm.java
+++ IntroDBProject/src/ch/ethz/inf/dbproject/forms/CommentForm.java
@@ -1,36 +1,22 @@
 package ch.ethz.inf.dbproject.forms;
 
-import static ch.ethz.inf.dbproject.util.Constant.WEB_ROOT;
-
-import java.io.IOException;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import org.apache.commons.lang3.StringUtils;
 
 import ch.ethz.inf.dbproject.forms.fields.Field;
 import ch.ethz.inf.dbproject.forms.fields.HiddenField;
 import ch.ethz.inf.dbproject.forms.fields.TextArea;
-import ch.ethz.inf.dbproject.model.CaseNote;
-import ch.ethz.inf.dbproject.model.DatastoreInterface;
-import ch.ethz.inf.dbproject.util.UserManagement;
+import ch.ethz.inf.dbproject.model.Note;
 
 
-public class CommentForm extends Form<CaseNote> {
+public abstract class CommentForm<T extends Note> extends Form<T> {
 
 	final String comment = "Comment";
-	final String caseId = "Case";
+	final String refId = "ReferenceId";
 
 	
-	private TextArea fieldComment = new TextArea(comment);
-	private HiddenField fieldCase = new HiddenField(caseId, false);
+	TextArea fieldComment = new TextArea(comment);
+	HiddenField fieldReference = new HiddenField(refId, false);
 
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	@Override
@@ -38,23 +24,13 @@ public class CommentForm extends Form<CaseNote> {
 		return (List<Field>) (List) Arrays.asList(
 				new Field[]{
 				fieldComment,
-				fieldCase
+				fieldReference
 		});
-	}
-	
-	@Override
-	protected CaseNote processNewForm(List<Field> fields, HashMap<String, String> values, HttpSession session) {
-		requireNotEmpty(comment, values);
-		
-		int caseId = Integer.valueOf(StringUtils.strip(fieldCase.parse(values)));
-		 String comment = StringUtils.strip(fieldComment.getValue(values));
-		
-		return new DatastoreInterface().insertComment(comment, caseId, UserManagement.getCurrentlyLoggedInUser(session).getUserid());
 	}
 
 	@Override
 	public List<Field> editFormFields() {
-		throw new UnsupportedOperationException("Cannot edit User");
+		throw new UnsupportedOperationException("Cannot edit Note");
 	}
 
 	@Override
@@ -64,19 +40,11 @@ public class CommentForm extends Form<CaseNote> {
 
 	@Override
 	protected String getNewFormTitle() {
-		return "Create new Case";
+		return "Create new Note";
 	}
 
 	@Override
 	protected String getEditFormTitle() {
-		throw new UnsupportedOperationException("Cannot edit User");
-	}
-
-
-	@Override
-	protected void onNewSuccess(CaseNote result, ServletContext servletContext,
-			HttpServletRequest request, HttpServletResponse response,
-			HttpSession session) throws ServletException, IOException {
-		response.sendRedirect(WEB_ROOT + "Case?id=" + result.getCaseId());
+		throw new UnsupportedOperationException("Cannot edit Note");
 	}
 }
