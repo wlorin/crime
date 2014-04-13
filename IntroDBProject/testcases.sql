82985ac7f6ae291a21904ebef63fbf445ee7191f
Nina Zinsli <ninazinsli@gmail.com>
2014-04-13 15:02:07 +0200

    Add Crime Category testcases

--- IntroDBProject/testcases.sql
+++ IntroDBProject/testcases.sql
@@ -1,12 +1,42 @@
+--
+-- Crime
+--
+INSERT INTO `Crime` (Crime)
+  values("Koerperverletzung");
+INSERT INTO `Crime` (Crime)
+  values("Mord");
+INSERT INTO `Crime` (Crime)
+  values("Diebstahl");
+INSERT INTO `Crime` (Crime)
+  values("Einbruch");
+INSERT INTO `Crime` (Crime)
+  values("Bankraub");
+  
+
   
 --
 -- Testdaten
 --  
-INSERT INTO `Case` (description, date, time, status, location)
-  values("Diebstahl", '2014-03-11', '12:31:00', "open", "Zurich");
+INSERT INTO `Case` (crimeId, date, time, status, location)
+  SELECT Crime.CrimeId, '2014-03-11', '12:31:00', "open", "Zurich"
+  FROM Crime
+  WHERE Crime.Crime = "Diebstahl";
 
-INSERT INTO `Case` (description, date, time, status, location)
-  values("Mord", '2014-03-11', '12:31:00', "open", "Basel");
+INSERT INTO `Case` (crimeId, date, time, status, location)
+  SELECT Crime.CrimeId, '2014-03-11', '12:31:00', "open", "Basel"
+  FROM Crime
+  WHERE Crime.Crime = "Mord";
   
-INSERT INTO `Case` (description, date, time, status, location)
-  values("Koerperverletzung", '2012-08-23', '23:00:00', "closed", "Aarau");  
+INSERT INTO `Case` (crimeId, date, time, status, location)
+  SELECT Crime.CrimeId, '2012-08-23', '23:00:00', "closed", "Aarau"
+  FROM Crime
+  WHERE Crime.Crime = "Koerperverletzung";  
+  
+INSERT INTO `Case` (crimeId, date, time, status, location)
+  SELECT Crime.CrimeId, '2014-01-13', '09:15:00', "open", "Zurich"
+  FROM Crime
+  WHERE Crime.Crime = "Bankraub";
+  
+  
+  
+  
