5ac715183ec1b202f4c2c506ba0ac1c613f92f74
Christian Vonrüti <christian@vonrueti.ch>
2014-04-13 22:34:22 +0200

    Add different note forms

---
+++ IntroDBProject/src/ch/ethz/inf/dbproject/forms/CaseNoteForm.java
@@ -0,0 +1,66 @@
+package ch.ethz.inf.dbproject.forms;
+
+import static ch.ethz.inf.dbproject.util.Constant.WEB_ROOT;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.commons.lang3.StringUtils;
+
+import ch.ethz.inf.dbproject.forms.fields.Field;
+import ch.ethz.inf.dbproject.forms.fields.HiddenField;
+import ch.ethz.inf.dbproject.forms.fields.TextArea;
+import ch.ethz.inf.dbproject.model.CaseNote;
+import ch.ethz.inf.dbproject.model.DatastoreInterface;
+import ch.ethz.inf.dbproject.util.UserManagement;
+
+
+public class CaseNoteForm extends CommentForm<CaseNote> {
+
+	final String comment = "Comment";
+	final String caseId = "Case";
+
+	
+	TextArea fieldComment = new TextArea(comment);
+	HiddenField fieldCase = new HiddenField(caseId, false);
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@Override
+	public List<Field> newFormFields() {
+		return (List<Field>) (List) Arrays.asList(
+				new Field[]{
+				fieldComment,
+				fieldCase
+		});
+	}
+	
+	@Override
+	protected CaseNote processNewForm(List<Field> fields, HashMap<String, String> values, HttpSession session) {
+		requireNotEmpty(comment, values);
+		
+		int caseId = Integer.valueOf(StringUtils.strip(fieldCase.parse(values)));
+		 String comment = StringUtils.strip(fieldComment.getValue(values));
+		
+		return new DatastoreInterface().insertComment(comment, caseId, UserManagement.getCurrentlyLoggedInUser(session).getUserid());
+	}
+
+	@Override
+	protected String getNewFormTitle() {
+		return "Create new case note";
+	}
+
+	@Override
+	protected void onNewSuccess(CaseNote result, ServletContext servletContext,
+			HttpServletRequest request, HttpServletResponse response,
+			HttpSession session) throws ServletException, IOException {
+		response.sendRedirect(WEB_ROOT + "Case?id=" + result.getCaseId());
+	}
+}
---
+++ IntroDBProject/src/ch/ethz/inf/dbproject/forms/PoINoteForm.java
@@ -0,0 +1,48 @@
+package ch.ethz.inf.dbproject.forms;
+
+import static ch.ethz.inf.dbproject.util.Constant.WEB_ROOT;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.commons.lang3.StringUtils;
+
+import ch.ethz.inf.dbproject.forms.fields.Field;
+import ch.ethz.inf.dbproject.forms.fields.HiddenField;
+import ch.ethz.inf.dbproject.forms.fields.TextArea;
+import ch.ethz.inf.dbproject.model.CaseNote;
+import ch.ethz.inf.dbproject.model.DatastoreInterface;
+import ch.ethz.inf.dbproject.model.PoINote;
+import ch.ethz.inf.dbproject.util.UserManagement;
+
+
+public class PoINoteForm extends CommentForm<PoINote> {
+
+	final String comment = "Comment";
+	final String caseId = "Case";
+
+	@Override
+	protected PoINote processNewForm(List<Field> fields, HashMap<String, String> values, HttpSession session) {
+		requireNotEmpty(comment, values);
+		
+		int caseId = Integer.valueOf(StringUtils.strip(fieldReference.parse(values)));
+		 String comment = StringUtils.strip(fieldComment.getValue(values));
+		
+		return new DatastoreInterface().insertPoINote(comment, caseId, UserManagement.getCurrentlyLoggedInUser(session).getUserid());
+	}
+
+	@Override
+	protected void onNewSuccess(PoINote result, ServletContext servletContext,
+			HttpServletRequest request, HttpServletResponse response,
+			HttpSession session) throws ServletException, IOException {
+		response.sendRedirect(WEB_ROOT + "PoIDetail?id=" + result.getCaseId());
+	}
+}
