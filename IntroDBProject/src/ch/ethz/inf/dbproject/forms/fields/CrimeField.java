0fe603bab92c91ba04623f290ad8eaa6792f89c5
Christian Vonrüti <christian@vonrueti.ch>
2014-04-14 10:28:26 +0200

    Implement Crime-Form-Field

---
+++ IntroDBProject/src/ch/ethz/inf/dbproject/forms/fields/CrimeField.java
@@ -0,0 +1,26 @@
+package ch.ethz.inf.dbproject.forms.fields;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ch.ethz.inf.dbproject.model.Crime;
+import ch.ethz.inf.dbproject.model.DatastoreInterface;
+
+
+public class CrimeField extends SelectField {
+
+	public CrimeField(String displayName) {
+		super(displayName, crimeItems());
+	}
+
+	private static String[] crimeItems() {
+		List<String> value__name = new ArrayList<String>();
+		List<Crime> all = new DatastoreInterface().getAll(Crime.class);
+		for (Crime category : all) {
+			value__name.add("" + category.getId());
+			value__name.add(category.getName());
+		}
+		
+		return value__name.toArray(new String[value__name.size()]);
+	}
+}
