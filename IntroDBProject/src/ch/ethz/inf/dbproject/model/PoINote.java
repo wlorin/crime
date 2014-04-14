929b5bdf72202a3e1feade517be97063be2e9a3a
Christian Vonrüti <christian@vonrueti.ch>
2014-04-14 11:44:36 +0200

    Add missing fields to PoINote

--- IntroDBProject/src/ch/ethz/inf/dbproject/model/PoINote.java
+++ IntroDBProject/src/ch/ethz/inf/dbproject/model/PoINote.java
@@ -6,14 +6,23 @@ import java.sql.SQLException;
 public class PoINote extends Note {
 	
 	final int poiId;
-
+	final private int id;
 	
 	public PoINote(ResultSet rs) throws SQLException {
 		super(rs);
 		poiId = rs.getInt("PoIId");
+		id = rs.getInt("PoINoteId");
+	}
+
+	public int getId() {
+		return id;
 	}
 
 	public int getCaseId() {
+		return poiId;
+	}
+
+	public int getPoiId() {
 		return poiId;
 	}
 }
